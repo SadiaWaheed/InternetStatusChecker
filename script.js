16e7d399ce06d3c8c48133495f1039689cabe185
Sadia Waheed <sadia.waheed312@hotmail.com>
2025-12-11 20:36:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", checkInternetConnection);
+
+function checkInternetConnection() {
+  const statusText = document.getElementById("statusText");
+  const ipAddressText = document.getElementById("ipAddressText");
+  const networkStrengthText = document.getElementById("networkStrengthText");
+
+  statusText.textContent = "Checking....";
+
+  if (navigator.onLine) {
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => {
+        ipAddressText.textContent = data.ip;
+        statusText.textContent = 'Connected';
+        const connection = navigator.connection;
+        networkStrengthText.textContent = connection? connection.downlink+'Mbps':'Unknown';
+      })
+      .catch(() => {
+        statusText.textContent = "Disconnected";
+        ipAddressText.textContent = "-";
+        networkStrengthText.textContent = "-";
+      });
+  } else {
+    statusText.textContent = "Disconnected";
+    ipAddressText.textContent = "-";
+    networkStrengthText.textContent = "-";
+  }
+}
